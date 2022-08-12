9dc9acfab527810c630591e1a772b6b44a6ff4f8
gechcom <getinetay@gmail.com>
2022-08-12 09:27:19 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,87 +1,86 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * execute - executes the command
- * @cmd: command to run
- * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ * main - provides a prompt to be interpreted by the shell
+ * @ac: number of items in av
+ * @av: NULL terminated array of strings representing inputted arguments
+ *
+ * Return: On success returns 0, on failure provides error number
  */
-int execute(char **cmd)
+int main(__attribute__((__unused__)) int ac, char **av)
 {
+	size_t len = 0;
+	ssize_t read = 0;
+	char *line = NULL;
+	char **args = NULL;
+	list_t *env = set_env_list();
+	int last_status;
+	status_t *status;
 
-	pid_t child_pid;
-	int status;
-
-	if (strncmp("exit", cmd[0], 4) == 0)
-		return (-1);
-
-	child_pid = fork();
-
-	if (child_pid == -1)
+	status = new_status(0, 0, av[0]);
+	signal(SIGINT, handle_sigint);
+	while (read != EOF)
 	{
-		perror("Error");
-		return (1);
-	}
-	else if (child_pid == 0)
-	{
-		if (execve(cmd[0], cmd, NULL) == -1)
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "<^^^> ", 6);
+		read = getline(&line, &len, stdin);
+		if (read != EOF)
 		{
-			perror("Error");
-			exit(-1);
+			status->count++;
+			remove_comments(line);
+			if (only_delims(line))
+				continue;
+			args = strtow(line, " \t\r\n\v\f");
+			replace_dollars(args, env, status);
+			if (_cmpstrandlen(args[0], "exit") == 0)
+				free(line);
+			status->last_status =
+				get_built_in(args[0])(args, env, status);
+			if (status->last_status == 0 ||
+			    status->last_status == 2)
+				continue;
+			if (!_strpbrk(args[0], '/'))
+				search_path(args, env);
+			status->last_status = execute(args, env, status);
 		}
 	}
-	else
-		wait(&status);
-
-	return (0);
+	last_status = status->last_status;
+	free(line), free_list(env), free_status(status);
+	return (last_status);
 }
 
+/**
+ * handle_sigint - Handle the signal Ctrl+C
+ * @sig: The signal
+ */
+void handle_sigint(int sig)
+{
+	(void) sig;
+	write(STDOUT_FILENO, "\n<^^^> ", 7);
+}
 
 /**
- * main - main simple shell
- * @argc: number of arguments
- * @argv: list of command line arguments
- * Return: Always 0, -1 on error.
+ * only_delims - Check if line consists of only delimiters
+ * @line: The line to check
+ *
+ * Return: 1 if true, 0 if false
  */
-
-int main(int argc, char **argv)
+int only_delims(char *line)
 {
+	int i, j, delim_found;
+	char delim[] = " \t\r\n\v\f";
 
-	int response;
-	char **tokens;
-	size_t bufsize = BUFSIZ;
-	int isPipe = 0;
-	char *buffer;
-
-	if (argc >= 2)
+	for (i = 0; line[i] != '\0'; i++)
 	{
-		/*TODO: Handle cases where there is no argument, only the command*/
-		if (execve(argv[1], argv, NULL) == -1)
+		delim_found = 0;
+		for (j = 0; delim[j] != '\0'; j++)
 		{
-			perror("Error");
-			exit(-1);
+			if (line[i] == delim[j])
+				delim_found = 1;
 		}
-		return (0);
+		if (!delim_found)
+			return (0);
 	}
 
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Unable to allocate buffer");
-		exit(1);
-	}
-
-	do {
-		if (isatty(fileno(stdin)))
-		{
-			isPipe = 1;
-			_puts("cisfun#: ");
-		}
-
-		getline(&buffer, &bufsize, stdin);
-		buffer[_strlen(buffer) - 1] = '\0';
-		tokens = stringToTokens(buffer);
-		response = execute(tokens);
-	} while (isPipe && response != -1);
-
-	return (0);
+	return (1);
 }
